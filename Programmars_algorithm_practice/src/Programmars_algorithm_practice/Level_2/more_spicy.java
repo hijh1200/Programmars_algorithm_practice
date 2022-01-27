975db18bf8e06ec37164142c7a6cbfa2b6464413
hijh1200 <hijh1200@naver.com>
2022-01-28 03:01:52 +0900

    더 맵게

--- Programmars_algorithm_practice/src/Programmars_algorithm_practice/Level_2/more_spicy.java
+++ Programmars_algorithm_practice/src/Programmars_algorithm_practice/Level_2/more_spicy.java
@@ -1,28 +1,32 @@
 package Programmars_algorithm_practice.Level_2;
 
+import java.util.Arrays;
 import java.util.PriorityQueue;
 public class more_spicy {
 	public static int solution(int[] scoville, int K) {
 		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-		for(int i=0; i<scoville.length; i++) pq.add(scoville[i]);
-		int count = 0;
+		for(int i=0; i<scoville.length; i++) {
+			pq.add(scoville[i]);
+		}
 		
-		if(pq.size() == 1) return count;
+		int cnt = 0;
 		while(true) {
-			if(pq.peek() > K) return count;
-			if(pq.size() == 1) return -1;
+			if(pq.peek() >= K) {
+				break;
+			}
+			else if(pq.size() == 1) {
+				return -1;
+			}
 			else {
-				if(pq.size() < 2) return count;
-				int a = pq.poll();
-				int b = pq.poll();
-				pq.add(a + b * 2);
-				count++;
+				pq.add(pq.poll() + pq.poll() * 2);
+				cnt++;
 			}
 		}
+		return cnt;
 	}
 
 	public static void main(String[] args) {
-		int[] scovile = {1, 2, 3, 9, 10, 12};
+		int[] scovile = {1,1};
 		int K = 7;
 		System.out.println(solution(scovile, K));
 	}
