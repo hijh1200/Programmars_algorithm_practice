81006a122266f363a2c44f78604f97424fe8dfe6
hijh1200 <hijh1200@naver.com>
2022-02-01 04:52:19 +0900

    프린터

---
+++ Programmars_algorithm_practice/src/Programmars_algorithm_practice/Level_2/light_path_cycle.java
@@ -0,0 +1,10 @@
+package Programmars_algorithm_practice.Level_2;
+
+public class light_path_cycle {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Programmars_algorithm_practice/src/Programmars_algorithm_practice/Level_2/printer.java
+++ Programmars_algorithm_practice/src/Programmars_algorithm_practice/Level_2/printer.java
@@ -2,19 +2,27 @@ package Programmars_algorithm_practice.Level_2;
 import java.util.*;
 public class printer {
 	public static int solution(int[] priorities, int location) {
-		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-		for(int i=0; i<priorities.length; i++) {
-			pq.add(priorities[i]);
-		}
-		System.out.println(pq.peek());
-		
-		
-		
-		
-		
-		
-		return 0;
-	}
+        int idx = location;
+        Queue<Integer> q = new LinkedList<Integer>();
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+        for(int i : priorities) {q.add(i); pq.add(i);}
+        int cnt = 0;
+        while(q.size() != 0) {
+            if(q.peek() == pq.peek()) {
+                pq.poll();
+                q.poll();
+                cnt++;
+                if(idx == 0) return cnt;
+            }
+            else {
+                int temp = q.poll();
+                q.add(temp);
+            }
+            if(idx == 0) idx = q.size() - 1;
+            else idx--;
+        }
+        return 0;
+    }
 
 	public static void main(String[] args) {
 		int [] priorities = {2, 1, 3, 2};
