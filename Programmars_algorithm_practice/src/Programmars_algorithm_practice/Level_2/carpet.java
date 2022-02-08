5ed5419b0f3edb4da665059d98fa045ebb05740d
hijh1200 <hijh1200@naver.com>
2022-02-08 22:09:15 +0900

    카펫

--- Programmars_algorithm_practice/src/Programmars_algorithm_practice/Level_2/carpet.java
+++ Programmars_algorithm_practice/src/Programmars_algorithm_practice/Level_2/carpet.java
@@ -1,31 +1,31 @@
 package Programmars_algorithm_practice.Level_2;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class carpet {
 	public static int[] solution(int brown, int yellow) {
-		ArrayList<Integer> a = new ArrayList<Integer>();
-		ArrayList<Integer> b = new ArrayList<Integer>();
-		
+		Queue<int[]> q = new LinkedList<>();
 		for(int i=1; i<=Math.sqrt(yellow); i++) {
-			if(yellow % i == 0) {
-				a.add(i);
-				b.add(yellow/i);
+			if(yellow%i == 0) {
+				q.add(new int[] {yellow/i, i});
 			}
 		}
-		
-		int[] res = new int[2];
-		for(int i=0; i<a.size(); i++) {
-			if(a.get(i) * 2 + b.get(i) * 2 + 4 == brown) {
-				res[0] = b.get(i) + 2;
-				res[1] = a.get(i) + 2;
+		while(!q.isEmpty()) {
+			int[] arr = q.poll();
+			if(arr[0]*2 + arr[1]*2 + 4 == brown) {
+				arr[0] += 2;
+				arr[1] += 2;
+				return arr;
 			}
 		}
+		int[] res = {};
 		return res;
 	}
 	public static void main(String[] args) {
-		int brown = 10;
-		int yellow = 2;
+		int brown = 24;
+		int yellow = 24;
 		System.out.println(solution(brown, yellow));
 	}
 
